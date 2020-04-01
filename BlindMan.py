d0cbb037f16867d19bdc69c948ce28036d18ce7d
lilsteelan <62084214+lilsteelan@users.noreply.github.com>
2020-04-02 10:59:26 +1100

    Update BlindMan.py

--- BlindMan.py
+++ BlindMan.py
@@ -1,8 +1,16 @@
-Version = 1.0
+Version = 1.2
 #Importations
 import time
 import turtle
 import random
 import datetime
+import sys
 
+"""DEFINITIONS"""
+def animate(var):
+    for x in var:
+        sys.stdout.write(x)
+        time.sleep(0.045)
+        sys.stdout.flush()
 
+animate("is this fluent? Test hello world")
