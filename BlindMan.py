63684a277f61502a17940e9f538ddce159fc07cd
lilsteelan <62084214+lilsteelan@users.noreply.github.com>
2020-04-02 11:37:57 +1100

    Update BlindMan.py

--- BlindMan.py
+++ BlindMan.py
@@ -1,10 +1,12 @@
+
 Version = 1.3
 #Importations
 import time
-import turtle as *
+from turtle import *
 import random
 import datetime
 import sys
+
 
 """DEFINITIONS"""
 def animate(var):
@@ -12,11 +14,20 @@ def animate(var):
         sys.stdout.write(x)
         time.sleep(0.045)
         sys.stdout.flush()
+"""VARIABLES"""
+coins = 100
+GameIsRunning = True
+while GameIsRunning:
+	animate("Welcome to the blind man run,\n")
+	animate("enter the maze at YOUR OWN Risk\n")
+	animate("btw, c = yes, and x = no\n")
+	cont = input("Continue: ")
+	print(cont)
+	#Check the users response
+	if cont == "c" or cont == " c":
+		animate("Very well then, as you wish\n")
 
-animate("Welcome to the blind man run,\n")
-animate("enter the maze at YOUR OWN Risk\n")
-animate("btw, c = yes, and x = no\n")
-cont = input("Continue?: ")
-
-if cont == c:
-	animate("Very well then, as you wish\n")
+	#If it did not match what we intended
+	else:
+		animate("Invalid answer, try again")
+		pass
