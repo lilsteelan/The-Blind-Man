d5130fa23fdcf986452db7cc68484dc6f15db6ee
SC-L-Logix <63025181+SC-L-Logix@users.noreply.github.com>
2020-04-02 11:15:48 +1100

    Update BlindMan.py

--- BlindMan.py
+++ BlindMan.py
@@ -1,7 +1,7 @@
-Version = 1.2
+Version = 1.3
 #Importations
 import time
-import turtle
+import turtle as *
 import random
 import datetime
 import sys
@@ -13,4 +13,10 @@ def animate(var):
         time.sleep(0.045)
         sys.stdout.flush()
 
-animate("is this fluent? Test hello world")
+animate("Welcome to the blind man run,\n")
+animate("enter the maze at YOUR OWN Risk\n")
+animate("btw, c = yes, and x = no\n")
+cont = input("Continue?: ")
+
+if cont == c:
+	animate("Very well then, as you wish\n")
